1c71628390bea74790fdd1dc0053a63db4a995a4
Gerard Fowley <gerard.fowley@iqeo.net>
2018-09-15 21:22:02 -0400

    phaser js test

--- www/js/index.js
+++ www/js/index.js
@@ -16,31 +16,78 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+// TODO: Phaser in JS to test setup .... convert to Ruby!
+
+function preload ()
+{
+  this.load.setBaseURL('http://localhost:8000/');
+  this.load.image('sky', 'img/space3.png');
+  this.load.image('logo', 'img/phaser3-logo.png');
+  this.load.image('red', 'img/red.png');
+}
+
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+  var particles = this.add.particles('red');
+  var emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  });
+  var logo = this.physics.add.image(400, 100, 'logo');
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+  emitter.startFollow(logo);
+}
+
+// TODO: convert this cordova stuff to Ruby ?
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    // TODO: Phaser in JS to test setup .... convert to Ruby!
+    var config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade',
+                 arcade: {
+                   gravity: { y: 200 }
+                 }
+      },
+      scene: {
+        preload: preload,
+        create: create
+      }
+    };
+    var game = new Phaser.Game(config);
+  },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    console.log('Received Event: ' + id);
+  }
 
-        console.log('Received Event: ' + id);
-    }
 };
 
 app.initialize();
+
